0dab5e855b1b07b2d1629f02cb82ead2dfed0ba0
Rana-Banerjee <rana1224@gmail.com>
2020-10-24 11:27:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import os
 from setuptools import dist,find_packages, setup
 
-dist.Distribution().fetch_build_eggs(['numpy','torch'])
+dist.Distribution().fetch_build_eggs(['torch'])
 
 import torch
 from torch.utils.cpp_extension import (BuildExtension, CppExtension,
